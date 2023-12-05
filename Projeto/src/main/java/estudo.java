feab586cbb6b3b25f3bd758720eaad9443089850
Willzera <113561581+Wssfilho@users.noreply.github.com>
2023-12-04 22:12:50 -0300

    terminar

--- Projeto/src/main/java/estudo.java
+++ Projeto/src/main/java/estudo.java
@@ -1,16 +1,10 @@
-import java.awt.BorderLayout;
-import static java.awt.Component.LEFT_ALIGNMENT;
-import java.awt.Container;
-import java.awt.Dimension;
-import java.awt.GridLayout;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import java.awt.*;
+
 import java.util.ArrayList;
 import javax.swing.BorderFactory;
 import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
-import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JList;
@@ -22,32 +16,71 @@ import javax.swing.JTextField;
 public class estudo extends JFrame{
 
     JLabel lbltitulo, lblinstrutor;
-    JLabel lbliniciodat, lblfimdat;
+    JLabel lbliniciodat, lblfimdat, lblListaCursos;
     JTextField txttitulo, txtintrutor, txtiniciodat, txtfimdat;
-    JList listodos;
+    JList jlistodos;
     DefaultListModel mdlListaClientes;
-    //ArrayList<cursos> ltscursos = new ArrayList<cursos>();
+    ArrayList<Cursos> ltscursos = new ArrayList<Cursos>();
+    public void NovoCurso()
+    {
+        String titulo, instrutor, InDat, outDat;
+        int iniciodat, fimdat;
+        InDat = txtiniciodat.getText();
+        outDat = txtfimdat.getText();
+        titulo = txttitulo.getText();
+        instrutor   = txtintrutor.getText();
+        iniciodat = Integer.parseInt(InDat);
+        fimdat = Integer.parseInt(outDat);
 
+        Cursos csos = new Cursos(titulo, instrutor, iniciodat, fimdat);
+        jlistodos.add(csos);
+
+        JOptionPane.showMessageDialog(rootPane, "Novo curso criado: " +
+                csos.titulo);
+
+        lblNumeroClientes.setText(" Total clientes : "+lstClientes.size());
+
+        AtualizarLista();
+    }
     estudo()
     {
+        lblListaCursos = new JLabel("Lista de cursos");
         lbltitulo = new JLabel("titular");
         txttitulo = new JTextField();
         lblinstrutor = new JLabel("Instrutor");
         txtintrutor = new JTextField();
-        lbliniciodat = new JLabel("iniciodata");
+        lbliniciodat = new JLabel("Data de inicio");
         txtiniciodat = new JTextField();
-        lblfimdat = new JLabel("fimdata");
+        lblfimdat = new JLabel("Data de fim");
         txtfimdat = new JTextField();
         mdlListaClientes = new DefaultListModel();
-        listodos = new JList(mdlListaClientes);
+        jlistodos = new JList(mdlListaClientes);
+
         JPanel formulario = new JPanel();
+        formulario.setBackground(Color.LIGHT_GRAY);
         formulario.setLayout(new GridLayout(4, 2));
+        formulario.setBorder(BorderFactory.createEmptyBorder(10, 10, 10, 10));
         formulario.add(lbltitulo);
         formulario.add(txttitulo);
         formulario.add(lblinstrutor);
         formulario.add(txtintrutor);
-
-
+        formulario.add(lbliniciodat);
+        formulario.add(txtiniciodat);
+        formulario.add(lblfimdat);
+        formulario.add(txtfimdat);
+        JScrollPane listScroller = new JScrollPane(jlistodos);
+        listScroller.setPreferredSize(new Dimension(250, 80));
+        listScroller.setAlignmentX(LEFT_ALIGNMENT);
+        JPanel listPane = new JPanel();
+        listPane.setBackground(Color.black);
+        listPane.setLayout(new BoxLayout(listPane, BoxLayout.PAGE_AXIS));
+        listPane.add(lblListaCursos);
+        listPane.add(Box.createRigidArea(new Dimension(0,5)));
+        listPane.add(listScroller);
+        listPane.setBorder(BorderFactory.createEmptyBorder(10,10,10,10));
+        Container contentPane = getContentPane();
+        contentPane.add(formulario, BorderLayout.NORTH);
+        contentPane.add(listPane, BorderLayout.CENTER);
     }
     public static void main(String[] args)
     {
@@ -57,8 +90,57 @@ public class estudo extends JFrame{
         a.setBounds(300, 300, 400, 400);
         a.setVisible(true);
     }
+    public class Cursos
+    {
+        /*JLabel lbltitulo, lblinstrutor;
+        JLabel lbliniciodat, lblfimdat, lblListaCursos;
+        JTextField txttitulo, txtintrutor, txtiniciodat, txtfimdat;
 
+         */
+        private String titulo, intrutor;
+        private int datin, datfim;
 
+        Cursos(String ti, String in, int dain, int daout)
+        {
+            this.titulo = ti;
+            this.intrutor = in;
+            this.datfim = daout;
+            this.datin = dain;
+
+        }
+
+        public int getDatfim() {
+            return datfim;
+        }
+
+        public int getDatin() {
+            return datin;
+        }
+
+        public String getIntrutor() {
+            return intrutor;
+        }
+
+        public String getTitulo() {
+            return titulo;
+        }
+
+        public void setDatfim(int datfim) {
+            this.datfim = datfim;
+        }
+
+        public void setDatin(int datin) {
+            this.datin = datin;
+        }
+
+        public void setIntrutor(String intrutor) {
+            this.intrutor = intrutor;
+        }
+
+        public void setTitulo(String titulo) {
+            this.titulo = titulo;
+        }
+    }
 
 }
 
